d5ea1436c497b70c208850dd6e09681e2fda06e0
Phillip Harris <phillip.harris@atlanticbt.com>
2015-03-16 10:38:57 -0400

    Initial Commit

---
+++ SevenColorsExampleTests/SevenColorsExampleTests.m
@@ -0,0 +1,40 @@
+//
+//  SevenColorsExampleTests.m
+//  SevenColorsExampleTests
+//
+//  Created by Phillip Harris on 3/16/15.
+//  Copyright (c) 2015 Phillip Harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface SevenColorsExampleTests : XCTestCase
+
+@end
+
+@implementation SevenColorsExampleTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
